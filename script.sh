3db43812404e502e8d215e86e29e53c7ac104130
Robert Kazaryan <kazaryanrobert@gmail.com>
2025-06-09 11:59:36 +0200

    Add Dockerfile, dependencies, definition and initialization script for Node-RED setup

---
+++ script.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+sleep 15
+mkdir -p $NODE_RED_DIRECTORY
+mkdir -p $NODE_RED_DIRECTORY/lib
+mkdir -p $NODE_RED_DIRECTORY/lib/flows
+mkdir -p $NODE_RED_DIRECTORY/lib/flows/oscar-subflows
+mkdir -p $NODE_RED_DIRECTORY/lib/flows/oscar-examples
+cp /oscar-subflows/* $NODE_RED_DIRECTORY/lib/flows/oscar-subflows
+cp /oscar-examples/* $NODE_RED_DIRECTORY/lib/flows/oscar-examples
+
+PASSWORD=${PASSWORD:-"admin"}
+NODE_RED_PWD_HASH=$(echo "$PASSWORD" | node-red admin hash-pw | cut -d' ' -f2)
+
+node-red --port 1880 --userDir $NODE_RED_DIRECTORY \
+  -D uiHost="::" \
+  -D httpRoot=$NODE_RED_BASE_URL \
+  -D credentialSecret=false \
+  -D contextStorage.default.module=localfilesystem \
+  -D adminAuth="{\"type\":\"credentials\",\"users\":[{\"username\":\"admin\",\"password\":\"$NODE_RED_PWD_HASH\",\"permissions\":\"*\"}]}" \
+  &
+
+while true; do
+  sleep 1
+done
